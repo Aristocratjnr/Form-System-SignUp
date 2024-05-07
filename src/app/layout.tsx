04053260ae3b2cde726fb448611cd8a226b30d6a
David Jnr <102563270+Aristocratjnr@users.noreply.github.com>
2024-05-07 12:59:51 +0000

    update of web title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "InkrisPilot",
+  title: "Form Management System",
   description: "Form Management System",
 };
 
